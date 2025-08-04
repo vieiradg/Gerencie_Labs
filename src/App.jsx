1a3bda7a249c7b4ed9c1117b34f4d5e168f32faa
Vieiradg <166573248+vieiradg@users.noreply.github.com>
2025-08-04 00:20:49 -0300

    first commit, a estrututa + estilização

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PaginaInicial from './components/PaginaInicial/PaginaInicial';
+import './global.css'; // Importa os estilos globais
+
+// O componente principal agora só mostra a página inicial.
+export default function App() {
+    return (
+        <PaginaInicial />
+    );
+}
