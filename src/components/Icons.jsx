8e32848a024ee5f27ee1bab4b0f55ac27e9f03d5
Vieiradg <166573248+vieiradg@users.noreply.github.com>
2025-08-17 20:16:55 -0300

    Ajuste
    Ajustei o icons porque deu erro no vercel

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Icone = ({ children, className }) => (
   <svg
-    xmlns="http:
+    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24"
+    viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
@@ -16,6 +16,7 @@ const Icone = ({ children, className }) => (
     {children}
   </svg>
 );
+
 export const IconeCasa = ({ className }) => (
   <Icone className={className}>
     <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
